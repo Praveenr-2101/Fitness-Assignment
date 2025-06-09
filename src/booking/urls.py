fc5b658fae34c6e670fb64768c5d8f3f2341f910
Praveenr-2101 <155968605+Praveenr-2101@users.noreply.github.com>
2025-06-09 14:20:29 +0530

    Delete src directory

--- src/booking/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ClassListView, BookingCreateView, BookingListView
-
-urlpatterns = [
-    path('classes/', ClassListView.as_view(), name='class-list'),
-    path('book/', BookingCreateView.as_view(), name='booking-create'),
-    path('bookings/', BookingListView.as_view(), name='booking-list'),
-]
